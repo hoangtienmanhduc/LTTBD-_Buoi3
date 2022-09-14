5115bafa0850ba0950584edae321b387afb701d3
Admin <khongcotinhban@gmail.com>
2022-09-15 01:53:06 +0700

    second commit

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-
+import { Image, ImageBackground, StyleSheet, Text, View} from "react-native";
+import { Button } from "@react-native-material/core";
 
 export default function App() {
   return (
     <View style={styles.main}>
-        {/* <Image
-          style={styles.tinyLogo}
-          source={require('/img/circle.png')}
-        /> */}
-        <Text style={styles.lable}>GROW {"\n"}YOUR BUSINESS</Text>
-        <Text style={styles.text}>We will help you to grow your business using online server</Text>
-        <Text style={styles.text1}>HOW WE WORK?</Text>
-    </View>
+    <ImageBackground source={require('./img/background.png')} style={styles.container}>
+      <Image
+        style={styles.tinyLogo}
+        source={require('./img/circle.png')}
+      />
+      <Text style={styles.lable}>GROW {"\n"}YOUR BUSINESS</Text>
+      <Text style={styles.text}>We will help you to grow your business using online server</Text>
+      <View style={styles.btn}>
+          <Button text='loggin' title="LOGIN" color="#E3C000"/>
+          <Button text='Sign up'title="SIGN UP" color="#E3C000"/>
+      </View>
+      <Text style={styles.text1}>HOW WE WORK?</Text>
+    </ImageBackground>
+  </View>
   );
 };
 
@@ -57,3 +62,4 @@ const styles = StyleSheet.create({
     width: 400,
   },
 });
+
